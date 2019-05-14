495dd3c8b267014ba84d05479fb3f351c9bd91d6
9plus <541004828@qq.com>
2019-05-14 20:11:24 +0800

    add plus-server

---
+++ demo/plus/plus-server/src/main/java/com.plus/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.plus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "forward:/index.html";
+    }
+}
