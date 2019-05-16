5d3ab03230e28ce0cb41578fd9fd5e53188ddd8b
9plus <541004828@qq.com>
2019-05-16 21:07:53 +0800

    git serializable

--- crawler-server/src/main/java/com/plus/model/PO/DanMuPo.java
+++ crawler-server/src/main/java/com/plus/model/PO/DanMuPo.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class DanMuPo implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private String id;
 
     private String time;
